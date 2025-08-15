33cf10261de9e7da757f926020f2778ce948ad2a
Dorji <sherpadorjee2580@gmail.com>
2025-08-15 10:06:33 +0545

    Solved merge confilct

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
 import CommunityHub from "./Components/CommunityHub/CommunityHub";
 import NewEvent from "./Components/NewEvent/NewEvent";
 import UserProfile from "./Components/UserProfile/UserProfile";
+import ExploreEvents from "./Components/ExploreEvents/ExploreEvents";
 
 const App = () => {
   return (
     <div>
-      {/* <CommunityHub /> */}
-      {/* <NewEvent />   */}
-      <UserProfile />
+      <ExploreEvents />
     </div>
   );
 };
-=======
-import "./App.css";
-import ExploreEvents from "./Components/ExploreEvents/ExploreEvents";
-
-function App() {
-  return (
-    <>
-      <ExploreEvents />
-    </>
-  );
-}
->>>>>>> 89abe3b0eeb1112a360e9ab54211f6c0378dc100
 
 export default App;
