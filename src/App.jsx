979744d60133d4c5981b96e92979fe2728956ef0
INDRA-011 <indr.yznn.77@gmail.com>
2025-08-15 10:19:31 +0545

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import CommunityHub from "./Components/CommunityHub/CommunityHub";
 import NewEvent from "./Components/NewEvent/NewEvent";
 import UserProfile from "./Components/UserProfile/UserProfile";
 import ExploreEvents from "./Components/ExploreEvents/ExploreEvents";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
     <div>
-      <ExploreEvents />
+      <Login />
     </div>
   );
 };
