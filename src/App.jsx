feaec9887c17833a094c74246193a8aeed199124
Dorji <sherpadorjee2580@gmail.com>
2025-08-15 10:22:04 +0545

    Added the Feed Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import CommunityHub from "./Components/CommunityHub/CommunityHub";
 import NewEvent from "./Components/NewEvent/NewEvent";
 import UserProfile from "./Components/UserProfile/UserProfile";
 import ExploreEvents from "./Components/ExploreEvents/ExploreEvents";
+import Feed from "./Components/Feed/Feed";
 
 const App = () => {
   return (
     <div>
-      <ExploreEvents />
+      <Feed />
     </div>
   );
 };
