64e592380b1a591f4f49151e23e5a52dad483ef5
Tshering592 <tsheringfunchok@gmail.com>
2025-08-15 11:10:38 +0545

    Solve the merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Nav from './Components/Nav/Nav';
 
-function App() {
-  const [count, setCount] = useState(0)
-=======
 import React from "react";
 import "./App.css";
 import CommunityHub from "./Components/CommunityHub/CommunityHub";
@@ -17,25 +8,17 @@ import ExploreEvents from "./Components/ExploreEvents/ExploreEvents";
 import Feed from "./Components/Feed/Feed";
 import Login from "./Components/Login/Login";
 import Sidebar from "./Components/Sidebar/Sidebar"; // Importing the Sidebar component
->>>>>>> 79facd757479fc24e2d5747c07079beac9ef7d23
+import Nav from "./Components/Nav/Nav"
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <>
-      <Nav/>
-        
-    </>
-  )
-}
-=======
     <div>
+      <Nav/>
       <Feed />
       <Login />
       <Sidebar />
     </div>
   );
 };
->>>>>>> 79facd757479fc24e2d5747c07079beac9ef7d23
 
 export default App;
